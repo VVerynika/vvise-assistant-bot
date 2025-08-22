09cc36eb0416209b533319feccdffcd654484aff
Veronika Karpyza <veronika.karpyza@gmail.com>
2025-08-22 13:47:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import clickup_monitor
 if __name__ == "__main__":
     Thread(target=bot.bot.polling, kwargs={'none_stop': True, 'interval': 0, 'timeout': 60}, daemon=True).start()
     Thread(target=slack_watcher.run, daemon=True).start()
-    Thread(target=clickup_monitor.run, daemon=True).start()
+    Thread(target=clickup_monitor.run_clickup_monitor, daemon=True).start()
     while True:
         time.sleep(3600)
